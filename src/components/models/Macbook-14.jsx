4a31c8acbdcba85ab8ac6cb0138b00b827d0e19d
Krupa Mehta <krupamehta2007@gmail.com>
2026-04-09 22:17:02 +0530

    Showcase section updated.

--- src/components/models/Macbook-14.jsx
+++ src/components/models/Macbook-14.jsx
@@ -13,6 +13,7 @@ import { useGLTF, useTexture } from '@react-three/drei'
 import useMacbookStore from '../../store'
 import { noChangeParts } from '../../constants';
 import * as THREE from 'three';
+import { SRGBColorSpace } from 'three';
 
 export default function MacbookModel14(props) {
   const {color} = useMacbookStore();
@@ -20,6 +21,8 @@ export default function MacbookModel14(props) {
   const { nodes, materials, scene } = useGLTF('/models/macbook-14-transformed.glb')
 
   const texture=useTexture('/screen.png')
+    texture.colorSpace = SRGBColorSpace;
+    texture.needsUpdate=true;
 
 useEffect(()=>{
   scene.traverse((child)=>{

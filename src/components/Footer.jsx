0211f4059e541a16be62dd5539386ada2fca7933
Krupa Mehta <krupamehta2007@gmail.com>
2026-04-11 19:13:52 +0530

    Implement footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { footerLinks } from '../constants'
+import { href } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>
-      ola
-    </div>
+    <footer>
+      <div className='info'>
+        <p> More ways to shop: Find an Apple or other retailer near you. Or call 000800 040 1966. </p>
+        <img src='/logo.svg' alt='Apple logo'/>
+
+      </div>
+
+      <hr/>
+      <div className='links'>
+        <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+
+
+        <ul>
+          {footerLinks.map(({label,link})=>{
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          })}
+        </ul>
+      </div>
+
+    </footer>
   )
 }
 
